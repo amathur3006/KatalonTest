74b4b1d6b7ccc231bfe6a76794758ea6c9cff149
amathur3006 <arpitmathur2020@gmail.com>
2025-03-21 01:03:17 +0530

    Commit4

---
+++ Object Repository/Page_Automation Practice - Ultimate QA/a_Big page with many elements.rs
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Big page with many elements</name>
+   <tag></tag>
+   <elementGuidId>f54bba1d-9a22-4187-993b-a73857e825e6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//article[@id='post-507']/div/div/div/div[2]/div/div/div/div/div/div/ul/li/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.et_pb_text_inner > ul > li > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Big page with many elements&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>108aadf7-d631-4831-a027-f72d3bbc83b4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>../complicated-page</value>
+      <webElementGuid>fa8e84bd-f17f-4c1c-bb2a-2f76929958f5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Big page with many elements</value>
+      <webElementGuid>147ce5ca-d931-4c5d-a57a-5e0108461279</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post-507&quot;)/div[@class=&quot;entry-content&quot;]/div[@class=&quot;et-l et-l--post&quot;]/div[@class=&quot;et_builder_inner_content et_pb_gutters2&quot;]/div[@class=&quot;et_pb_section et_pb_section_1 et_pb_with_background et_section_specialty&quot;]/div[@class=&quot;et_pb_row et_pb_row_3-4_1-4&quot;]/div[@class=&quot;et_pb_column et_pb_column_3_4 et_pb_column_1   et_pb_specialty_column  et_pb_css_mix_blend_mode_passthrough&quot;]/div[@class=&quot;et_pb_row_inner et_pb_row_inner_0&quot;]/div[@class=&quot;et_pb_column et_pb_column_4_4 et_pb_column_inner et_pb_column_inner_0 et-last-child&quot;]/div[@class=&quot;et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_light&quot;]/div[@class=&quot;et_pb_text_inner&quot;]/ul[1]/li[1]/a[1]</value>
+      <webElementGuid>ef3e645a-c44f-47d8-b6e6-017eaff5690d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//article[@id='post-507']/div/div/div/div[2]/div/div/div/div/div/div/ul/li/a</value>
+      <webElementGuid>d1f87ee3-1318-4e5b-9e81-16993942696e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Big page with many elements')]</value>
+      <webElementGuid>9c4a56b4-4385-4808-beae-50904fd0003d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Use your skills to learn how to automate different scenarios'])[1]/following::a[1]</value>
+      <webElementGuid>df67f47e-ec89-41cd-91fa-2efdff2679e0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Automation Practice'])[1]/following::a[1]</value>
+      <webElementGuid>6c22d063-0a3a-473f-a305-edf59724c8b3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fake Landing Page'])[1]/preceding::a[1]</value>
+      <webElementGuid>9146f65b-4acd-4668-aa97-0d4fc6e238c9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fake Pricing Page'])[1]/preceding::a[2]</value>
+      <webElementGuid>83ffa0df-0582-4c7e-99f2-2dbad68b1b36</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Big page with many elements']/parent::*</value>
+      <webElementGuid>673cec48-416f-42e7-a426-6e7e19b594d4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '../complicated-page')]</value>
+      <webElementGuid>4acc6ce0-1317-4e26-a041-dd17f115c3ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/div/ul/li/a</value>
+      <webElementGuid>a83a94de-9302-456b-b5d0-037798267d83</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '../complicated-page' and (text() = 'Big page with many elements' or . = 'Big page with many elements')]</value>
+      <webElementGuid>65b5c110-d146-47dc-b8e4-d0fc08dd3730</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
